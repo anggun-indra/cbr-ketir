321a9c53822d789aaaaf1ded1458ebf3d8d2bb95
anggun indra <anggun.indra@melon.co.id>
2018-11-17 20:43:26 +0700

    initial commit cbr-ketir

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,18 +3,41 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
+//use Illuminate\Support\Facades\Schema;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
+//    /**
+//     * Bootstrap any application services.
+//     *
+//     * @return void
+//     */
+//    public function boot()
+//    {
+//        Schema::defaultStringLength(191);
+//    }
+//
+//    /**
+//     * Register any application services.
+//     *
+//     * @return void
+//     */
+//    public function register()
+//    {
+//        //
+//    }
+
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        Schema::defaultStringLength(191);
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
     }
 
     /**
@@ -24,6 +47,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 }
